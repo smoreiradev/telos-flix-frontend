96d622d49613054aead8e865d2fc4364d2969509
Lucas Souza <lucassouzafranco1@gmail.com>
2023-05-31 10:11:50 -0300

    ✨ feat(coment card): component for comments in movie details page

---
+++ src/components/comentCard/index.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './index.css'
+import RatingStarsCommentCard from '../RattingStarsCommentCard'
+
+
+function commentCard({user, comment, rate}) {
+  return (
+    <div className='CommentCard'>
+        <div className='CommentCardContent'>
+        <h3>{user}</h3>
+        <p>{comment}</p>
+        <div className='RatingNoteDiv'>
+        <h5>{rate}</h5>
+        <RatingStarsCommentCard value={rate}/>
+        </div>
+        </div>
+    </div>
+  )
+}
+
+export default commentCard

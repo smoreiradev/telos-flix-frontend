d3dfe76c592fae95e8d882700b4db0d31ebeb002
Lucas Souza <lucassouzafranco1@gmail.com>
2023-06-11 19:31:05 -0300

    ♻️ refactor(header): solving code conflicts

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,5 +1,4 @@
 import { useContext, useEffect, useState } from "react";
-
 import { styled, useTheme } from "@mui/material/styles";
 import Box from "@mui/material/Box";
 import MuiDrawer from "@mui/material/Drawer";
@@ -23,6 +22,13 @@ import CustomModal from "../customModal";
 import LoginModalContent from "../loginModalContent";
 import CreateAccountModalContent from "../createAccountModalContent";
 import { AuthContext } from "../../contexts/AuthContext";
+import Button from "@mui/material/Button";
+import Avatar from "@mui/material/Avatar";
+import Grid from "@mui/material/Grid";
+import AccountCircle from "@mui/icons-material/AccountCircle";
+import Logout from "@mui/icons-material/Logout";
+import { Typography } from "@mui/material";
+
 const drawerWidth = 240;
 
 const openedMixin = (theme) => ({
@@ -35,6 +41,7 @@ const openedMixin = (theme) => ({
   }),
   overflowX: "hidden",
 });
+
 const closedMixin = (theme) => ({
   border: "none",
   backgroundColor: "#212121",
@@ -48,6 +55,7 @@ const closedMixin = (theme) => ({
     width: `calc(${theme.spacing(8)} + 1px)`,
   },
 });
+
 const DrawerHeader = styled("div")(({ theme }) => ({
   display: "flex",
   alignItems: "center",
@@ -56,6 +64,7 @@ const DrawerHeader = styled("div")(({ theme }) => ({
   // necessary for content to be below app bar
   ...theme.mixins.toolbar,
 }));
+
 const AppBar = styled(MuiAppBar, {
   shouldForwardProp: (prop) => prop !== "open",
 })(({ theme, open }) => ({
@@ -92,16 +101,45 @@ const Drawer = styled(MuiDrawer, {
   }),
 }));
 
+const StyledButton = styled(Button)(({ theme }) => ({
+  color: "#eeeeee",
+  fontWeight: "600",
+  padding: "8px 16px",
+}));
+
+const ProfileComponent = ({ user, onLogout }) => {
+  return (
+    <StyledButton>
+      <Grid container spacing={1} alignItems="center">
+        <Grid item>
+          <Avatar>{user?.name?.charAt(0)}</Avatar>
+        </Grid>
+        <Grid item>
+          <IconButton onClick={onLogout}>
+            <Logout style={{marginRight: '-50px', color: '#eeeeee', opacity: '0.2'}}/>
+          </IconButton>
+        </Grid>
+      </Grid>
+    </StyledButton>
+  );
+};
+
 export default function Header() {
   const theme = useTheme();
   const [open, setOpen] = useState(false);
-  const [contentToShow, setContentToShow] = useState(<></>);
-  const { savedUser } = useContext(AuthContext);
+  const [contentToShow, setContentToShow] = useState();
+  const { storedUser, setStoredUser } = useContext(AuthContext);
+
+  const handleLogout = () => {
+    // Perform logout logic here
+    setStoredUser(null);
+  };
+
   useEffect(() => {
-    if (savedUser) {
+    if (storedUser) {
       setOpen(false);
     }
-  }, [savedUser]);
+  }, [storedUser]);
 
   return (
     <Box sx={{ display: "flex" }}>
@@ -111,11 +149,19 @@ export default function Header() {
           sx={{
             display: "flex",
             justifyContent: "space-between",
+            position: "relative",
           }}
-        >
-          <img src={logo} alt="logo" />
-          {savedUser?.name}
-          {!savedUser?.name && (
+        > 
+        <Box sx={{ display: "flex", alignItems: "center" }}>
+            <img src={logo} alt="logo" />
+            <Typography variant="h6" sx={{ fontWeight: "500", ml: 1, marginTop: 0 }}>
+                TelosFlix
+              </Typography>
+        </Box>
+          {storedUser?.name && (
+            <ProfileComponent user={storedUser} onLogout={handleLogout} />
+          )}
+          {!storedUser?.name && (
             <AppBarActions
               actions={[
                 <CreateAccountButton
@@ -143,7 +189,7 @@ export default function Header() {
       </AppBar>
       <Drawer variant="permanent">
         <DrawerHeader>
-          <IconButton sx={{ color: "#fff" }}>
+          <IconButton sx={{ color: "#fff" }} onClick={() => setOpen(!open)}>
             {theme.direction === "rtl" ? (
               <ChevronRightIcon />
             ) : (

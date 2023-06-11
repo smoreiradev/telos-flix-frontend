1e817640916a795c5afb8a5c94a3f5ebc24fed6e
Lucas Souza <lucassouzafranco1@gmail.com>
2023-06-10 22:56:56 -0300

    ✨ feat(main video): play telos opening when application starts
    Uses react-player lib to play the main video on home page

--- src/components/mainVideo/index.jsx
+++ src/components/mainVideo/index.jsx
@@ -1,9 +1,48 @@
 import React from "react";
-import image from "./Hero.png";
+import ReactPlayer from "react-player";
+
 function MainVideo() {
+  const width = '1070px';
+  const height = '600px';
+  const videoURL = 'https://youtu.be/JTPIJFzvpfY';
+
+  const playerConfig = {
+    youtube: {
+      playerVars: {
+        modestbranding: 1,
+      },
+    },
+  };
+
   return (
-    <div style={{marginTop:"284px"}}>
-      <img src={image}></img>
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginBottom:'90px'
+      }}
+    >
+      <div
+        style={{
+          borderRadius: '10px',
+          overflow: 'hidden',
+          boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.2)',
+        }}
+      >
+        {videoURL ? (
+          <ReactPlayer
+            url={videoURL}
+            width={width}
+            height={height}
+            controls
+            playing
+            config={playerConfig}
+          />
+        ) : (
+          <p>Loading...</p>
+        )}
+      </div>
     </div>
   );
 }
